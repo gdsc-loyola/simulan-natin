f83d4301ae958218168919596cd207d7c89fab28
AlVanilla <al.bomediano@obf.ateneo.edu>
2022-04-15 12:29:39 +0800

    deleted strapi project from repo

--- simulan-natin-strapi/src/api/representative/routes/representative.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * representative router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::representative.representative');
